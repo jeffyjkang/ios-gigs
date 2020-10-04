e345ac11397419e3432e71e8178d79ad57c4a3eb
jeff kang <jeffkang@Jeffs-MacBook-Pro.local>
2020-10-04 09:27:00 -0500

    day1 mvp

---
+++ gigs/gigs/Models/Bearer.swift
@@ -0,0 +1,12 @@
+//
+//  Bearer.swift
+//  gigs
+//
+//  Created by Jeff Kang on 10/3/20.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
---
+++ gigs/gigs/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  gigs
+//
+//  Created by Jeff Kang on 10/3/20.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
